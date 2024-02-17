18a4f6b5b5aeaaeebdd77a2715ec15c30cdcbf03
MEIYAZHAGANgit <meiyazhaganmailbox@gmail.com>
2024-02-17 16:14:17 +0530

    first commit

---
+++ models/portfolioModels.js
@@ -0,0 +1,97 @@
+const mongoose = require("mongoose");
+
+const introSchema = new mongoose.Schema({
+  welcomeText: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  caption: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const aboutSchema = new mongoose.Schema({
+  lottieURL: {
+    type: String,
+    required: true,
+  },
+  description1: {
+    type: String,
+    required: true,
+  },
+  description2: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: Array,
+    required: true,
+  },
+});
+
+const EducationSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  period: {
+    type: String,
+    required: true,
+  },
+  institute: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  gmail: {
+    type: String,
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = {
+  Intro: mongoose.model("intros", introSchema),
+  About: mongoose.model("abouts", aboutSchema),
+  Education: mongoose.model("Education,EducationSchema"),
+  Contact: mongoose.model("contacts", contactSchema),
+};
---
+++ routes/portfolioRoutes.js
@@ -0,0 +1,108 @@
+const router = require("express").Router();
+const {
+  Intro,
+  About,
+  Project,
+  Contact
+} = require("../models/portfolioModel");
+const User = require("../models/userModel");
+// get all portfolio data
+router.get("/get-portfolio-data", async (req, res) => {
+  try {
+    const intros = await Intro.find();
+    const abouts = await About.find();
+    const projects = await Project.find();
+    const contacts = await Contact.find();
+   
+    res.status(200).send({
+      intro: intros[0],
+      about: abouts[0],
+      projects: projects,
+      contact: contacts[0],
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// update intro
+router.post("/update-intro", async (req, res) => {
+  try {
+    const intro = await Intro.findOneAndUpdate(
+      { _id: req.body._id },
+      req.body,
+      { new: true }
+    );
+    res.status(200).send({
+      data: intro,
+      success: true,
+      message: "Intro updated successfully",
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// update about
+router.post("/update-about", async (req, res) => {
+  try {
+    const about = await About.findOneAndUpdate(
+      { _id: req.body._id },
+      req.body,
+      { new: true }
+    );
+    res.status(200).send({
+      data: about,
+      success: true,
+      message: "Abouts updated successfully",
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+
+// update contact
+router.post("/update-contact", async (req, res) => {
+  try {
+    const contact = await Contact.findOneAndUpdate(
+      { _id: req.body._id },
+      req.body,
+      { new: true }
+    );
+    res.status(200).send({
+      data: contact,
+      success: true,
+      message: "Contact updated successfully",
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// admin login
+router.post("/admin-login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    user.password = "";
+    if (user) {
+      res.status(200).send({
+        data: user,
+        success: true,
+        message: "Login successfully",
+      });
+    } else {
+      res.status(200).send({
+        data: user,
+        success: false,
+        message: "Invalid username or password",
+      });
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+module.exports = router;
